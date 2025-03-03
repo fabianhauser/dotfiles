2a091f3b59d50f8746895650a9520d143119af9f
Fabian Hauser <fabian@fh2.ch>
2025-03-03 15:28:07 +0200

    Make zed dark again

--- home-modules/applications/zed-editor.nix
+++ home-modules/applications/zed-editor.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   programs.zed-editor = {
     enable = true;
@@ -24,30 +24,6 @@
       nodePackages.cspell
       direnv
     ];
-    userSettings = {
-      assistant = {
-        default_model = {
-          provider = "openai";
-          model = "o1-mini";
-        };
-        version = "2";
-      };
-      vim_mode = false;
-      ui_font_size = 16;
-      buffer_font_size = 16;
-      theme = {
-        mode = "system";
-        light = "Andromeda";
-        dark = "One Dark";
-      };
-      telemetry = {
-        diagnostics = false;
-        metrics = false;
-      };
-      languages = {
-        Markdown.remove_trailing_whitespace_on_save = false;
-        Markdown-Inline.remove_trailing_whitespace_on_save = false;
-      };
-    };
+    userSettings = lib.importJSON ./zed-editor/settings.json;
   };
 }
